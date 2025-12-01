0656952ebef8696c084dbd620de29611439416d6
Pathmasri E <pathmasri1372005@gmail.com>
2025-12-01 18:51:52 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 
-import React from "react";
+import React,{useState}from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const[name,setName]=useState("")
   return (
     <div>
         {/* Do not remove the main div */}
+    <input type="text" placeholder="Enter Your Name" value={name}onChange={(e)=>setName(e.target.value)}/>
+{name<p>Hello,{name}!</p>}
     </div>
   )
 }
